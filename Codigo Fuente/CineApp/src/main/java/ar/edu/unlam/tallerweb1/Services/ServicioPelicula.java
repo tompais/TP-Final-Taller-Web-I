71861d7bf04ab4b7d7fc2cf78f0c48c348fe89ea
Tomás Joaquín Pais <tomas.j.pais@gmail.com>
2019-07-06 04:01:58 -0300

    Migro funciones de calificación a servicioPelicula

--- Codigo Fuente/CineApp/src/main/java/ar/edu/unlam/tallerweb1/Services/ServicioPelicula.java
+++ Codigo Fuente/CineApp/src/main/java/ar/edu/unlam/tallerweb1/Services/ServicioPelicula.java
@@ -1,6 +1,7 @@
 package ar.edu.unlam.tallerweb1.Services;
 
 import ar.edu.unlam.tallerweb1.Exceptions.PeliculaNoEncontradaException;
+import ar.edu.unlam.tallerweb1.Models.Cine;
 import ar.edu.unlam.tallerweb1.Models.Pelicula;
 
 import java.util.List;
@@ -10,4 +11,6 @@ public interface ServicioPelicula {
     List<Pelicula> getPeliculasDisponiblesEnCartelera();
     List<Pelicula> getProximosEstrenos();
     Pelicula getPeliculaById(Long peliculaId) throws PeliculaNoEncontradaException;
+    List<Cine> getCinesDisponiblesByPeliculaId(Long peliculaId);
+    Double getPromedioCalificacionByPeliculaId(Long id);
 }
--- Codigo Fuente/CineApp/src/main/java/ar/edu/unlam/tallerweb1/Services/ServicioPeliculaImpl.java
+++ Codigo Fuente/CineApp/src/main/java/ar/edu/unlam/tallerweb1/Services/ServicioPeliculaImpl.java
@@ -2,13 +2,17 @@ package ar.edu.unlam.tallerweb1.Services;
 
 import ar.edu.unlam.tallerweb1.Enums.CodigoError;
 import ar.edu.unlam.tallerweb1.Exceptions.PeliculaNoEncontradaException;
+import ar.edu.unlam.tallerweb1.Models.Cine;
 import ar.edu.unlam.tallerweb1.Models.Pelicula;
 import ar.edu.unlam.tallerweb1.dao.PeliculaCineDao;
 import ar.edu.unlam.tallerweb1.dao.PeliculaDao;
+import ar.edu.unlam.tallerweb1.dao.PeliculaUsuarioDao;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.inject.Inject;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.Comparator;
 import java.util.List;
 
@@ -21,6 +25,9 @@ public class ServicioPeliculaImpl implements ServicioPelicula {
 
     @Inject
     private PeliculaCineDao peliculaCineDao;
+
+    @Inject
+    private PeliculaUsuarioDao peliculaUsuarioDao;
 
     @Override
     public List<Pelicula> getPeliculasDisponiblesEnCartelera() {
@@ -46,5 +53,17 @@ public class ServicioPeliculaImpl implements ServicioPelicula {
             throw new PeliculaNoEncontradaException("No se ha encontrado la película solicitada", CodigoError.PELICULANOENCONTRADA);
 
         return pelicula;
+    }
+
+    @Override
+    public List<Cine> getCinesDisponiblesByPeliculaId(Long peliculaId) {
+        return peliculaCineDao.getCinesDisponiblesByPeliculaId(peliculaId);
+    }
+
+    @Override
+    public Double getPromedioCalificacionByPeliculaId(Long id) {
+        Double promedioCalificacion = peliculaUsuarioDao.getPromedioCalificacionByPeliculaId(id);
+
+        return promedioCalificacion == null ? 0 : new BigDecimal(promedioCalificacion).setScale(1, RoundingMode.HALF_UP).doubleValue();
     }
 }

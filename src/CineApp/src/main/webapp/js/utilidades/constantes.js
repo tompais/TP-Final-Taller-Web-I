f2e4464c574eaba236ef6c4e0f14284edd0189b0
Tomás Joaquín Pais <tomas.j.pais@gmail.com>
2019-07-13 18:07:11 -0300

    Agrego constantes de estado de asiento

--- src/CineApp/src/main/webapp/js/utilidades/constantes.js
+++ src/CineApp/src/main/webapp/js/utilidades/constantes.js
@@ -11,3 +11,7 @@ const regexLetrasYNumeros = /^[0-9a-zA-Z]+$/;
 const regexSoloNumeros = /^[0-9]+$/;
 const regexLetrasYEspacio = /^[a-zA-ZÀ-ÿ\u00f1\u00d1]+(\s*[a-zA-ZÀ-ÿ\u00f1\u00d1]*)*[a-zA-ZÀ-ÿ\u00f1\u00d1]+$/;
 const regexLetrasNumerosYEspacio = /^[0-9a-zA-ZÀ-ÿ\u00f1\u00d1]+(\s*[0-9a-zA-ZÀ-ÿ\u00f1\u00d1]*)*[0-9a-zA-ZÀ-ÿ\u00f1\u00d1]+$/;
+
+const asientoLibre = 1;
+const asientoOcupado = 2;
+const asientoReservado = 3;

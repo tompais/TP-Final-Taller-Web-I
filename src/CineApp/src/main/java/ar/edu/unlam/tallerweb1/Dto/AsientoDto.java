9507aba53c63fac9048372c59c5b5526926309bb
Tomás Joaquín Pais <tomas.j.pais@gmail.com>
2019-07-16 23:57:13 -0300

    Agrego AsientoDto

---
+++ src/CineApp/src/main/java/ar/edu/unlam/tallerweb1/Dto/AsientoDto.java
@@ -0,0 +1,22 @@
+package ar.edu.unlam.tallerweb1.Dto;
+
+public class AsientoDto {
+    private Integer fila;
+    private Character columna;
+
+    public Integer getFila() {
+        return fila;
+    }
+
+    public void setFila(Integer fila) {
+        this.fila = fila;
+    }
+
+    public Character getColumna() {
+        return columna;
+    }
+
+    public void setColumna(Character columna) {
+        this.columna = columna;
+    }
+}
